d5772654237408af07578c00f295c80feeadb6fd
Miguel Ardila <miguel_sxe@hotmail.com>
2023-10-12 17:15:50 -0500

    sections - app.ts

---
+++ sections/app.ts
@@ -0,0 +1,44 @@
+// let numberOfLines = 1;
+// console.log("linea #", numberOfLines);
+
+// numberOfLines = numberOfLines + 1;
+// console.log("linea #", numberOfLines);
+
+// numberOfLines++;
+// console.log("linea #", numberOfLines);
+
+// numberOfLines += 1;
+// console.log("linea #", numberOfLines);
+
+// imprimir numero de linea
+
+
+let numberOfLines = 1;
+ export function increaseLineNumber(){
+    // numberOfLines++;
+    // numberOfLines++;
+    numberOfLines += 2;
+};
+
+console.log("linea #", numberOfLines);
+
+increaseLineNumber();
+console.log("linea #", numberOfLines);
+
+increaseLineNumber();
+console.log("linea #", numberOfLines);
+
+increaseLineNumber();
+console.log("linea #", numberOfLines);
+
+//-----------------------------------
+let numberOfLine = 0;
+function printLineNumber(){
+    numberOfLine ++; 
+    console.log("linea #", numberOfLine);
+}
+
+printLineNumber();
+printLineNumber();
+printLineNumber();
+printLineNumber();
