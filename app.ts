55cfc9df2d051ecc59f1aae99de81dba2462b9da
Miguel Ardila <miguel_sxe@hotmail.com>
2023-10-11 21:35:54 -0500

    practice

--- app.ts
+++ app.ts
@@ -1,2 +1,20 @@
+// let numberOfLines = 1;
+// console.log("linea #", numberOfLines);
 
-console.log("Hello world!");
+// numberOfLines = numberOfLines + 1;
+// console.log("linea #", numberOfLines);
+
+// numberOfLines++;
+// console.log("linea #", numberOfLines);
+
+// numberOfLines += 1;
+// console.log("linea #", numberOfLines);
+
+// imprimir numero de linea
+
+let numberOfLines = 1;
+function printLineNumber(){
+    numberOfLines ++;
+    console.log("linea #", numberOfLines);
+}
+
