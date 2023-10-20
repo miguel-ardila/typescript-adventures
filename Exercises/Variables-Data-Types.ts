6749fff1285a717979cc7b90c939198da0a9909c
Miguel Ardila <miguel_sxe@hotmail.com>
2023-10-19 22:11:13 -0500

    Variables-Data-Types

---
+++ Exercises/Variables-Data-Types.ts
@@ -0,0 +1,9 @@
+// Declaración de variables
+let nombre: string = "Juan";
+let edad: number = 30;
+let esEstudiante: boolean = true;
+
+// Uso de variables
+console.log(`Nombre: ${nombre}`);
+console.log(`Edad: ${edad}`);
+console.log(`¿Es estudiante? ${esEstudiante ? 'Sí' : 'No'}`);
