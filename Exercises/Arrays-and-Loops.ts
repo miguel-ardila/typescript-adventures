8868ed133d9d6a6976234e902b2c550297bf69f9
Miguel Ardila <miguel_sxe@hotmail.com>
2023-10-19 22:11:39 -0500

    Arrays-and-Loops

---
+++ Exercises/Arrays-and-Loops.ts
@@ -0,0 +1,8 @@
+// Descripción: Crear un arreglo y utilizar un bucle for para imprimir sus elementos.
+// Declaración de un arreglo
+const colores: string[] = ["Rojo", "Verde", "Azul"];
+
+// Iteración a través del arreglo
+for (let i = 0; i < colores.length; i++) {
+    console.log(`Color ${i + 1}: ${colores[i]}`);
+}
