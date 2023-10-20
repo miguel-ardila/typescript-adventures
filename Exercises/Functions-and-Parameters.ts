a0a36fd2190adddbaaff10921c3b11b5527a388f
Miguel Ardila <miguel_sxe@hotmail.com>
2023-10-19 22:13:11 -0500

    Functions-and-Parameters

---
+++ Exercises/Functions-and-Parameters.ts
@@ -0,0 +1,8 @@
+// Declaración de una función que suma dos números
+function sumar(a: number, b: number): number {
+    return a + b;
+}
+
+// Uso de la función
+const resultado = sumar(5, 3);
+console.log(`La suma es: ${resultado}`);
